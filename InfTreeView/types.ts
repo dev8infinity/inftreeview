2e93d601609c7460b8a68c4bdf83c5ee3d33c220
dev8infinity <dev8infinity@gmail.com>
2024-12-11 14:45:30 -0300

    melhorias de tipos

---
+++ InfTreeView/types.ts
@@ -0,0 +1,16 @@
+
+export interface InfTreeViewProps {
+    data: Array<{ [key: string]: any, expanded?: boolean }>,
+    fields?: { id?: string, text?: string, children?: string },
+    onNodeSelected?: (text: string) => void,
+    onNodeUpdate?: (newText: string) => void,
+}
+export interface InfTreeViewMethods {
+    expandAll: () => void;
+    collapseAll: () => void;
+    getJson: () => Object;
+    getData: () => Object;
+    updateSelectedNodeText: (text: string) => void;
+    updateFromJson: (jsonString: string) => boolean;
+}
+export type NestedObject = { [key: string]: string | NestedObject };
